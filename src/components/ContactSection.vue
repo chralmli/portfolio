7979f9256664a2724435a0026c95d784256b6c1d
Christian Almli <chrisalm97@gmail.com>
2023-12-16 01:24:56 +0100

    Project cards enhancements, added github link

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -4,15 +4,15 @@
             <h2 class="text-4x font-bold text-center text-gray-800 mb-10">Contact Me</h2>
             <form @submit.prevent="submitForm" class="max-w-lg mx-auto">
             <div class="mb-6">
-                <input v-model="formData.name" type="text" placeholder="Your Name" class="form-input">
+                <input v-model="formData.name" type="text" placeholder="Your Name" class="form-input" data-aos="fade-right" data-aos-delay="100">
             </div>
             <div class="mb-6">
-                <input v-model="formData.email" type="email" placeholder="Your Email" class="form-input">
+                <input v-model="formData.email" type="email" placeholder="Your Email" class="form-input" data-aos="fade-right" data-aos-delay="300">
             </div>
             <div class="mb-6">
-                <textarea v-model="formData.message" placeholder="Your Message" class="form-textarea"></textarea>
+                <textarea v-model="formData.message" placeholder="Your Message" class="form-textarea" data-aos="fade-right" data-aos-delay="600"></textarea>
             </div>
-            <button type="submit" class="submit-btn">Send Message</button>
+            <button type="submit" class="submit-btn" data-aos="fade-up" data-aos-delay="400">Send Message</button>
         </form>
         </div>
     </section>
--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -4,12 +4,15 @@
             <h2 class="text-4xl font-bold text-center text-gray-800 mb-10">My Work</h2>
 
             <!-- Projects Grid -->
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-10" data-aos="fade-up">
                 <div v-for="project in projects" :key="project.id" class="project-card transition-transform duration-500 ease-in-out transform hover:rotate-6 group cursor-pointer overflow-hidden relative shadow-lg rounded-2xl" @mouseover="rotateCard" @mouseleave="resetCard" :style="{ perspective: '1500px' }">
                     <img :src="project.image" alt="Project" :class="{'scale-105': isHovered}" :style="isHovered ? 'filter: brightness(70%)' : ''" class="transform group-hover:scale-110 transition-transform transition-filter duration-500 ease-in-out">
                     <div class="absolute project-title inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out text-xl font-semibold">
-                        <span class="project-title absolute inset-0 z-10 opacity-0 group-hover:opacity-100 text-xl font-medium text-white">{{  project.title }}</span>
-                        <a :href="project.url" target="_blank" class="view-project-btn hidden group-hover:flex transition-all duration-300 text-white py-2 px-5 rounded z-20">View Project</a>
+                        <span class="project-title top-4 left-4 absolute inset-0 z-10 opacity-0 group-hover:opacity-100 text-xl font-medium text-white">{{  project.title }}</span>
+                        <div class="flex space-x-4 mt-2">
+                            <a :href="project.url" target="_blank" class="view-project-btn hidden group-hover:flex transition-all duration-300 text-white py-2 px-5 rounded z-20">View Project</a>
+                            <a :href="project.repo" target="_blank" class="repo-link hidden group-hover:flex transition-all duration-300 text-white py-2 px-5 rounded z-20">Github</a>
+                        </div>
                     </div>
                     <p class="mt-2 text-center">{{  project.description }}</p>
                 </div>
@@ -37,6 +40,7 @@ export default {
                     description: 'A brief description of The Groove Grid.',
                     image: grooveGridImage,
                     url: 'https://taupe-medovik-72ed9d.netlify.app/',
+                    repo: 'https://github.com/chralmli/project-exam-1-chralmli',
                 },
                 {
                     id: 2,
@@ -44,13 +48,15 @@ export default {
                     description: 'A brief description of GameHub.',
                     image: gamehubImage,
                     url: 'https://storied-medovik-be5681.netlify.app/',
+                    repo: 'https://github.com/chralmli/cross-course-project-gamehub/tree/gamehub-cms-ca',
                 },
                 {
                     id: 3,
                     title: 'Community Science Museum',
                     description: 'A brief description of Community Science Museum.',
                     image: csmImage,
-                    url: 'https://elaborate-speculoos-c50810.netlify.app/'
+                    url: 'https://elaborate-speculoos-c50810.netlify.app/',
+                    repo: 'https://github.com/chralmli/semester_project-1/tree/master',
                 },
             ],
             selectedProject: null,
@@ -71,8 +77,16 @@ export default {
 
 @layer {
 
+    .view-project-btn, .repo-link {
+        @apply flex justify-center items-center text-white py-2 px-5 rounded transition-transform duration-300 z-20;
+    }
+
     .view-project-btn {
-        @apply flex justify-center items-center inset-0 opacity-0 scale-110 absolute transition-transform duration-300 m-auto;
+        @apply bg-indigo-600 hover:bg-indigo-500;
+    }
+
+    .repo-link {
+        @apply bg-gray-700 hover:bg-gray-600;
     }
 
     .project-card:hover .view-project-btn {
@@ -85,6 +99,10 @@ export default {
 
     .project-card:hover::after {
         @apply opacity-100;
+    }
+
+    .project-title {
+        @apply absolute top-4 left-4 z-10 opacity-0 group-hover:opacity-100 text-xl font-medium text-white;
     }
 }
 
